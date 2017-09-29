35ebd425a3524d2595e0dc378b172c8cc4715ab5
taggaddaaaa <sabinecaizergues@gmail.com>
2017-09-29 02:21:08 +0200

    Added 1 tweets

---
+++ data/js/tweets/2017_09.js
@@ -0,0 +1,112 @@
+Grailbird.data.tweets_2017_09 = 
+[
+	{
+		"created_at": "Thu Sep 28 05:05:05 +0000 2017",
+		"id": 913268300582412300,
+		"id_str": "913268300582412288",
+		"text": "@_100DaysOfCode hi! i m studying react native right now and was wondering : what's this # means ? #100DaysOfCode ? So I investigate :)",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "100DaysOfCode",
+					"indices": [
+						98,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_100DaysOfCode",
+					"name": "100daysofCode",
+					"id": 744776302591303700,
+					"id_str": "744776302591303680",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 913266975064887300,
+		"in_reply_to_status_id_str": "913266975064887296",
+		"in_reply_to_user_id": 744776302591303700,
+		"in_reply_to_user_id_str": "744776302591303680",
+		"in_reply_to_screen_name": "_100DaysOfCode",
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "paris",
+			"description": "student @42born2code",
+			"url": "https://t.co/PcMLKFnWl5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PcMLKFnWl5",
+							"expanded_url": "http://www.sabinecaizergues.me",
+							"display_url": "sabinecaizergues.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 295,
+			"listed_count": 11,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 48,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 128,
+			"lang": "fr",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	}
+]
