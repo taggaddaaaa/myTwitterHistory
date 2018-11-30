bb512d457f2ebd4ad60d57d5b000b3f2a0d57e80
taggaddaaaa <sabinecaizergues@hotmail.com>
2018-11-30 01:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 178,
-	"created_at": "Wed Apr 18 00:21:06 +0000 2018",
+	"tweets": 179,
+	"created_at": "Fri Nov 30 00:21:06 +0000 2018",
 	"lang": "fr"
 }
---
+++ data/js/tweets/2018_11.js
@@ -0,0 +1,128 @@
+Grailbird.data.tweets_2018_11 = 
+[
+	{
+		"created_at": "Thu Nov 29 23:06:17 +0000 2018",
+		"id": 1068280011491283000,
+		"id_str": "1068280011491282945",
+		"full_text": "The #ValidationBoard just launched! FREE tool to test startup ideas without wasting time or money. https://t.co/hNfBqFHqYo via @Lean",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ValidationBoard",
+					"indices": [
+						4,
+						20
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Lean",
+					"name": "Lean Startup Machine",
+					"id": 162887009,
+					"id_str": "162887009",
+					"indices": [
+						127,
+						132
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/hNfBqFHqYo",
+					"expanded_url": "http://leanstartupmachine.com/validationboard",
+					"display_url": "leanstartupmachine.com/validationboard",
+					"indices": [
+						99,
+						122
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "paris",
+			"description": "student @42born2code",
+			"url": "https://t.co/PcMLKFnWl5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PcMLKFnWl5",
+							"expanded_url": "http://www.sabinecaizergues.me",
+							"display_url": "sabinecaizergues.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97,
+			"friends_count": 292,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 49,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 132,
+			"lang": "fr",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The #ValidationBoard just launched! FREE tool to test startup ideas without wasting time or money. https://t.co/hNfBqFHqYo via @Lean"
+	}
+]
