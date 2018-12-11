9d478d854a933c7cf4a9bb954bc0001e5bc1197d
taggaddaaaa <sabinecaizergues@hotmail.com>
2018-12-11 01:51:24 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 179,
-	"created_at": "Fri Nov 30 00:21:06 +0000 2018",
+	"tweets": 180,
+	"created_at": "Tue Dec 11 00:51:23 +0000 2018",
 	"lang": "fr"
 }
---
+++ data/js/tweets/2018_12.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2018_12 = 
+[
+	{
+		"created_at": "Mon Dec 10 16:36:55 +0000 2018",
+		"id": 1072168294298341400,
+		"id_str": "1072168294298341378",
+		"full_text": "Finding bootstrap templates is easy @bootstrapzero https://t.co/XzBjyAcvKG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BootstrapZero",
+					"name": "BootstrapZero",
+					"id": 2245746949,
+					"id_str": "2245746949",
+					"indices": [
+						36,
+						50
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XzBjyAcvKG",
+					"expanded_url": "https://www.bootstrapzero.com/bootstrap-template/chameleon-admin-lite",
+					"display_url": "bootstrapzero.com/bootstrap-temp…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "paris",
+			"description": "student @42born2code",
+			"url": "https://t.co/PcMLKFnWl5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PcMLKFnWl5",
+							"expanded_url": "http://www.sabinecaizergues.me",
+							"display_url": "sabinecaizergues.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97,
+			"friends_count": 292,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 49,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 133,
+			"lang": "fr",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Finding bootstrap templates is easy @bootstrapzero https://t.co/XzBjyAcvKG"
+	}
+]
