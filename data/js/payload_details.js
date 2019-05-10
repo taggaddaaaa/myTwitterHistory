6dcf54d4207c7793d1df295ec6c19cabf65efc86
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-05-10 02:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 183,
-	"created_at": "Mon Apr 01 00:21:06 +0000 2019",
+	"tweets": 184,
+	"created_at": "Fri May 10 00:21:06 +0000 2019",
 	"lang": "fr"
 }
---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,215 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Thu May 09 07:52:41 +0000 2019",
+		"id": 1126394543480475600,
+		"id_str": "1126394543480475648",
+		"full_text": "5 jobs que j'ai fait:\n1. Coiffeuse/coloriste\n2. Commerciale en porte à porte\n3. Ambulancière DE\n4. Gérante d'un salon de thé/glacier\n5. Developpeuse Front-end...\nYouhouuuuuuuuuuuu! en 6, Je monte ma startup ? 😁 https://t.co/jZEZIpVEk7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jZEZIpVEk7",
+					"expanded_url": "https://twitter.com/_NicolasGtr/status/1121016887654125571",
+					"display_url": "twitter.com/_NicolasGtr/st…",
+					"indices": [
+						211,
+						234
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97,
+			"friends_count": 349,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 56,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 138,
+			"lang": "fr",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1121016887654125600,
+		"quoted_status_id_str": "1121016887654125571",
+		"quoted_status_permalink": {
+			"url": "https://t.co/jZEZIpVEk7",
+			"expanded": "https://twitter.com/_NicolasGtr/status/1121016887654125571",
+			"display": "twitter.com/_NicolasGtr/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Apr 24 11:43:48 +0000 2019",
+			"id": 1121016887654125600,
+			"id_str": "1121016887654125571",
+			"full_text": "5 jobs que j'ai fait:\n1. Manutentionnaire\n2. Plongeur... dans une cuisine\n3. Vendeur chez Go Sports\n4. Développeur\n5. Leader offers Microsoft &amp; Regional service center\n\n(Si quelqu'un comprend le dernier, merci de me l'expliquer...🤔) https://t.co/dy3pjsaXbf",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				236
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/dy3pjsaXbf",
+						"expanded_url": "https://twitter.com/HuguesOuvrard/status/1120966142531473409",
+						"display_url": "twitter.com/HuguesOuvrard/…",
+						"indices": [
+							237,
+							260
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 920272246047952900,
+				"id_str": "920272246047952896",
+				"name": "Nicolas Gtr",
+				"screen_name": "_NicolasGtr",
+				"location": "",
+				"description": "",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 22,
+				"friends_count": 61,
+				"listed_count": 3,
+				"created_at": "Tue Oct 17 12:56:16 +0000 2017",
+				"favourites_count": 842,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 330,
+				"lang": "fr",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/983350744718901248/Ox1PhqtW_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/983350744718901248/Ox1PhqtW_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/920272246047952896/1539615983",
+				"profile_link_color": "333640",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1120966142531473400,
+			"quoted_status_id_str": "1120966142531473409",
+			"quoted_status_permalink": {
+				"url": "https://t.co/dy3pjsaXbf",
+				"expanded": "https://twitter.com/HuguesOuvrard/status/1120966142531473409",
+				"display": "twitter.com/HuguesOuvrard/…"
+			},
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "5 jobs que j'ai fait:\n1. Coiffeuse/coloriste\n2. Commerciale en porte à porte\n3. Ambulancière DE\n4. Gérante d'un salon de thé/glacier\n5. Developpeuse Front-end...\nYouhouuuuuuuuuuuu! en 6, Je monte ma startup ? 😁 https://t.co/jZEZIpVEk7"
+	}
+]
