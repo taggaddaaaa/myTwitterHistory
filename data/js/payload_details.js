b13ec7ab0b6ea6daa06d92e872c00bffc5548242
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-06-08 02:21:07 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 187,
-	"created_at": "Thu May 30 00:21:07 +0000 2019",
+	"tweets": 189,
+	"created_at": "Sat Jun 08 00:21:06 +0000 2019",
 	"lang": "fr"
 }
---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,447 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Fri Jun 07 16:34:10 +0000 2019",
+		"id": 1137035027936686100,
+		"id_str": "1137035027936686081",
+		"full_text": "Voilà qui me redonne envie de me remettre sur framer ! Sympa le package! https://t.co/LA1XUaQIG0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LA1XUaQIG0",
+					"expanded_url": "https://twitter.com/MengTo/status/1137013177928048640",
+					"display_url": "twitter.com/MengTo/status/…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 101,
+			"friends_count": 388,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 119,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1137013177928048600,
+		"quoted_status_id_str": "1137013177928048640",
+		"quoted_status_permalink": {
+			"url": "https://t.co/LA1XUaQIG0",
+			"expanded": "https://twitter.com/MengTo/status/1137013177928048640",
+			"display": "twitter.com/MengTo/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 07 15:07:20 +0000 2019",
+			"id": 1137013177928048600,
+			"id_str": "1137013177928048640",
+			"full_text": "This Mockmatic component by @djfumberger is insanely cool. https://t.co/XVtYKqgZkG https://t.co/xQUgo5Wl4A",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				82
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "djfumberger",
+						"name": "David Fumberger",
+						"id": 167798177,
+						"id_str": "167798177",
+						"indices": [
+							28,
+							40
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/XVtYKqgZkG",
+						"expanded_url": "https://store.framer.com/package/djfumberger/mockmatic",
+						"display_url": "store.framer.com/package/djfumb…",
+						"indices": [
+							59,
+							82
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1137012888491700200,
+						"id_str": "1137012888491700225",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1137012888491700225/pu/img/CuZRzptkhPTjj3HS.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1137012888491700225/pu/img/CuZRzptkhPTjj3HS.jpg",
+						"url": "https://t.co/xQUgo5Wl4A",
+						"display_url": "pic.twitter.com/xQUgo5Wl4A",
+						"expanded_url": "https://twitter.com/MengTo/status/1137013177928048640/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1137012888491700200,
+						"id_str": "1137012888491700225",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1137012888491700225/pu/img/CuZRzptkhPTjj3HS.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1137012888491700225/pu/img/CuZRzptkhPTjj3HS.jpg",
+						"url": "https://t.co/xQUgo5Wl4A",
+						"display_url": "pic.twitter.com/xQUgo5Wl4A",
+						"expanded_url": "https://twitter.com/MengTo/status/1137013177928048640/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 57900,
+							"variants": [
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1137012888491700225/pu/vid/1280x720/h4ivKB7NMn3qyYZE.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1137012888491700225/pu/pl/vNblKIZUUqFd0WTO.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1137012888491700225/pu/vid/640x360/NB1Pw43_K5x_qpXt.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1137012888491700225/pu/vid/480x270/BRqdtCjeWMg6ak7F.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 5797422,
+				"id_str": "5797422",
+				"name": "Meng To",
+				"screen_name": "MengTo",
+				"location": "Montreal",
+				"description": "I design, code and write.",
+				"url": "https://t.co/KfC6jcnNZk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/KfC6jcnNZk",
+								"expanded_url": "http://designcode.io",
+								"display_url": "designcode.io",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 40087,
+				"friends_count": 310,
+				"listed_count": 1225,
+				"created_at": "Sun May 06 00:22:13 +0000 2007",
+				"favourites_count": 22483,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17655,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1098613826709348353/_9_0Nd2X_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1098613826709348353/_9_0Nd2X_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/5797422/1511997807",
+				"profile_link_color": "0099D1",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "1A1A1A",
+				"profile_text_color": "9E9E9E",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 136,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Voilà qui me redonne envie de me remettre sur framer ! Sympa le package! https://t.co/LA1XUaQIG0"
+	},
+	{
+		"created_at": "Fri Jun 07 16:32:30 +0000 2019",
+		"id": 1137034610200723500,
+		"id_str": "1137034610200723456",
+		"full_text": "@github Ça c'est de la super feature qui va me faire gagner un temps fou ! 😍",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "github",
+					"name": "GitHub",
+					"id": 13334762,
+					"id_str": "13334762",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1136671651540738000,
+		"in_reply_to_status_id_str": "1136671651540738048",
+		"in_reply_to_user_id": 13334762,
+		"in_reply_to_user_id_str": "13334762",
+		"in_reply_to_screen_name": "github",
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 101,
+			"friends_count": 388,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 119,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "fr",
+		"text": "@github Ça c'est de la super feature qui va me faire gagner un temps fou ! 😍"
+	}
+]
