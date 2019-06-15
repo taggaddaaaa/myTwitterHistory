e92d7d5c4d23a1a09f92bccda9f8f4a8f19f4506
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-06-15 02:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 189,
-	"created_at": "Sat Jun 08 00:21:06 +0000 2019",
+	"tweets": 190,
+	"created_at": "Sat Jun 15 00:21:06 +0000 2019",
 	"lang": "fr"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 14 15:28:00 +0000 2019",
+		"id": 1139555091277594600,
+		"id_str": "1139555091277594625",
+		"full_text": "Annonce CDI Montpellier: CHEF DE PROJETS EVENEMENTIEL (H/F) - Montpellier\nhttps://t.co/HOc6iBLIQg \nCompte à suivre : @MontpellierJobs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MontpellierJobs",
+					"name": "Emplois Montpellier",
+					"id": 1515012108,
+					"id_str": "1515012108",
+					"indices": [
+						117,
+						133
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HOc6iBLIQg",
+					"expanded_url": "http://www.francesurf.net/offre-emploi-chef-de-projets-evenementiel-montpellier_12156220.htm",
+					"display_url": "francesurf.net/offre-emploi-c…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://job.francesurf.net/\" rel=\"nofollow\">la recherche d'emploi par km</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 102,
+			"friends_count": 389,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 76,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 120,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Annonce CDI Montpellier: CHEF DE PROJETS EVENEMENTIEL (H/F) - Montpellier\nhttps://t.co/HOc6iBLIQg \nCompte à suivre : @MontpellierJobs"
+	},
+	{
 		"created_at": "Fri Jun 07 16:34:10 +0000 2019",
 		"id": 1137035027936686100,
 		"id_str": "1137035027936686081",
