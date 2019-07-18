be5e5f78f0d11f3c2524ccb2f7fc05ab559b0472
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-07-18 02:21:07 +0200

    Added 1 tweets

---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Wed Jul 17 10:16:36 +0000 2019",
+		"id": 1151435525313704000,
+		"id_str": "1151435525313703937",
+		"full_text": "@LDLC Amiga 500 miss it!",
+		"truncated": false,
+		"display_text_range": [
+			6,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LDLC",
+					"name": "LDLC",
+					"id": 28302970,
+					"id_str": "28302970",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1150117691824304100,
+		"in_reply_to_status_id_str": "1150117691824304128",
+		"in_reply_to_user_id": 28302970,
+		"in_reply_to_user_id_str": "28302970",
+		"in_reply_to_screen_name": "LDLC",
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 102,
+			"friends_count": 395,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 94,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 122,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@LDLC Amiga 500 miss it!"
+	}
+]
