947c8da717517e217544ac8e6f549294d284860d
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-08-28 02:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 195,
-	"created_at": "Sun Aug 11 00:21:06 +0000 2019",
+	"tweets": 196,
+	"created_at": "Wed Aug 28 00:21:06 +0000 2019",
 	"lang": "fr"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 27 17:00:38 +0000 2019",
+		"id": 1166395105403691000,
+		"id_str": "1166395105403691008",
+		"full_text": "Annonce CDI Montpellier: Développeur C#.NET F/H\nhttps://t.co/6Z4sM4MO6K \nSuivez : @MontpellierJobs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MontpellierJobs",
+					"name": "Emplois Montpellier",
+					"id": 1515012108,
+					"id_str": "1515012108",
+					"indices": [
+						82,
+						98
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6Z4sM4MO6K",
+					"expanded_url": "http://www.francesurf.net/offre-emploi-developpeur-c_14916852.htm",
+					"display_url": "francesurf.net/offre-emploi-d…",
+					"indices": [
+						48,
+						71
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://job.francesurf.net/\" rel=\"nofollow\">la recherche d'emploi par km</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 105,
+			"friends_count": 404,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 103,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 124,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Annonce CDI Montpellier: Développeur C#.NET F/H\nhttps://t.co/6Z4sM4MO6K \nSuivez : @MontpellierJobs"
+	},
+	{
 		"created_at": "Sat Aug 10 13:11:48 +0000 2019",
 		"id": 1160176926365012000,
 		"id_str": "1160176926365011968",
