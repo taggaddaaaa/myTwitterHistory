c053aacbaba78f03ea6fd7d1b8b4f4f4885bdd15
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-09-15 02:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 198,
-	"created_at": "Fri Sep 13 00:21:06 +0000 2019",
+	"tweets": 199,
+	"created_at": "Sun Sep 15 00:21:06 +0000 2019",
 	"lang": "fr"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 14 06:29:03 +0000 2019",
+		"id": 1172759142878109700,
+		"id_str": "1172759142878109696",
+		"full_text": "@LDLC Avec @LDLC j'aurais peut être un #1smartphonePourLaRentree ! https://t.co/MrsZCnCVb7",
+		"truncated": false,
+		"display_text_range": [
+			6,
+			66
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1smartphonePourLaRentree",
+					"indices": [
+						39,
+						64
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LDLC",
+					"name": "LDLC",
+					"id": 28302970,
+					"id_str": "28302970",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "LDLC",
+					"name": "LDLC",
+					"id": 28302970,
+					"id_str": "28302970",
+					"indices": [
+						11,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1172759129682784300,
+					"id_str": "1172759129682784257",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EEZ6NwPWsAE_WXI.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EEZ6NwPWsAE_WXI.jpg",
+					"url": "https://t.co/MrsZCnCVb7",
+					"display_url": "pic.twitter.com/MrsZCnCVb7",
+					"expanded_url": "https://twitter.com/taggaddaaaa/status/1172759142878109696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 376,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 376,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 376,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1172759129682784300,
+					"id_str": "1172759129682784257",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EEZ6NwPWsAE_WXI.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EEZ6NwPWsAE_WXI.jpg",
+					"url": "https://t.co/MrsZCnCVb7",
+					"display_url": "pic.twitter.com/MrsZCnCVb7",
+					"expanded_url": "https://twitter.com/taggaddaaaa/status/1172759142878109696/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 376,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 376,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 376,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							188
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EEZ6NwPWsAE_WXI.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1172416608842551300,
+		"in_reply_to_status_id_str": "1172416608842551297",
+		"in_reply_to_user_id": 28302970,
+		"in_reply_to_user_id_str": "28302970",
+		"in_reply_to_screen_name": "LDLC",
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 425,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 107,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 127,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "@LDLC Avec @LDLC j'aurais peut être un #1smartphonePourLaRentree ! https://t.co/MrsZCnCVb7"
+	},
+	{
 		"created_at": "Thu Sep 12 21:50:55 +0000 2019",
 		"id": 1172266362334584800,
 		"id_str": "1172266362334584835",
