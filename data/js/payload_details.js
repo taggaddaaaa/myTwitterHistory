ad7f6d2e1d33d12c670011ef9bfbd863ffa56fc2
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-09-29 02:21:07 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 199,
-	"created_at": "Sun Sep 15 00:21:06 +0000 2019",
+	"tweets": 201,
+	"created_at": "Sun Sep 29 00:21:06 +0000 2019",
 	"lang": "fr"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,515 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 28 07:40:16 +0000 2019",
+		"id": 1177583670984609800,
+		"id_str": "1177850496670982144",
+		"full_text": "🎁 #Concours\n Une trottinette électrique Ecogyro GScooter S8 à remporter : https://t.co/7vUlhL475C\n \n🔸 Pour tenter votre chance :\nRT + TWEET avec #FrenchDaysCdiscount \n\nTAS 04/10 \nRèglement : https://t.co/cm3pnKNoPK https://t.co/QF1kfiOT3R",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Concours",
+					"indices": [
+						2,
+						11
+					]
+				},
+				{
+					"text": "FrenchDaysCdiscount",
+					"indices": [
+						145,
+						165
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7vUlhL475C",
+					"expanded_url": "http://bit.ly/2mbqinC",
+					"display_url": "bit.ly/2mbqinC",
+					"indices": [
+						74,
+						97
+					]
+				},
+				{
+					"url": "https://t.co/cm3pnKNoPK",
+					"expanded_url": "http://bit.ly/2mFN5Z1",
+					"display_url": "bit.ly/2mFN5Z1",
+					"indices": [
+						191,
+						214
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1177583422371483600,
+					"id_str": "1177583422371483649",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/EFed4i_XUAEolKf.png",
+					"media_url_https": "https://pbs.twimg.com/media/EFed4i_XUAEolKf.png",
+					"url": "https://t.co/QF1kfiOT3R",
+					"display_url": "pic.twitter.com/QF1kfiOT3R",
+					"expanded_url": "https://twitter.com/Cdiscount/status/1177583670984609794/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 880,
+							"h": 440,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 880,
+							"h": 440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1177583422371483600,
+					"id_str": "1177583422371483649",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/EFed4i_XUAEolKf.png",
+					"media_url_https": "https://pbs.twimg.com/media/EFed4i_XUAEolKf.png",
+					"url": "https://t.co/QF1kfiOT3R",
+					"display_url": "pic.twitter.com/QF1kfiOT3R",
+					"expanded_url": "https://twitter.com/Cdiscount/status/1177583670984609794/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 880,
+							"h": 440,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 880,
+							"h": 440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pickaw.com\" rel=\"nofollow\">Pickaw</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 63142684,
+			"id_str": "63142684",
+			"name": "Cdiscount",
+			"screen_name": "Cdiscount",
+			"location": "France",
+			"description": "N'économisez pas votre plaisir",
+			"url": "https://t.co/IUhLib3sxX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IUhLib3sxX",
+							"expanded_url": "http://bit.ly/Cdiscount-Site",
+							"display_url": "bit.ly/Cdiscount-Site",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 402112,
+			"friends_count": 5559,
+			"listed_count": 826,
+			"created_at": "Wed Aug 05 14:12:44 +0000 2009",
+			"favourites_count": 4162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 101947,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "180882",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1151821335158505472/Z3ng21yO_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1151821335158505472/Z3ng21yO_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/63142684/1569561135",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10809,
+		"favorite_count": 3425,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "🎁 #Concours\n Une trottinette électrique Ecogyro GScooter S8 à remporter : https://t.co/7vUlhL475C\n \n🔸 Pour tenter votre chance :\nRT + TWEET avec #FrenchDaysCdiscount \n\nTAS 04/10 \nRèglement : https://t.co/cm3pnKNoPK https://t.co/QF1kfiOT3R"
+	},
+	{
+		"created_at": "Sat Sep 28 07:39:52 +0000 2019",
+		"id": 1177175903291478000,
+		"id_str": "1177850395047149568",
+		"full_text": "#Concours 🎁\n\nTente de gagner ce mega Pack Dofus !  Pour participer : RT + FOLLOW @LeStreamFR 👌\n\nTAS : 03/10 https://t.co/MK3CKSHMJ8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			107
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Concours",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeStreamFR",
+					"name": "LeStream",
+					"id": 710885582780629000,
+					"id_str": "710885582780628996",
+					"indices": [
+						81,
+						92
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1177175897310466000,
+					"id_str": "1177175897310466053",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EFYrPd1XkAUzv4a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFYrPd1XkAUzv4a.jpg",
+					"url": "https://t.co/MK3CKSHMJ8",
+					"display_url": "pic.twitter.com/MK3CKSHMJ8",
+					"expanded_url": "https://twitter.com/LeStreamFR/status/1177175903291478017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1177175897310466000,
+					"id_str": "1177175897310466053",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EFYrPd1XkAUzv4a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFYrPd1XkAUzv4a.jpg",
+					"url": "https://t.co/MK3CKSHMJ8",
+					"display_url": "pic.twitter.com/MK3CKSHMJ8",
+					"expanded_url": "https://twitter.com/LeStreamFR/status/1177175903291478017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1177175897515933700,
+					"id_str": "1177175897515933696",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EFYrPemWwAAN2YJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFYrPemWwAAN2YJ.jpg",
+					"url": "https://t.co/MK3CKSHMJ8",
+					"display_url": "pic.twitter.com/MK3CKSHMJ8",
+					"expanded_url": "https://twitter.com/LeStreamFR/status/1177175903291478017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1177175897314603000,
+					"id_str": "1177175897314603008",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EFYrPd2WsAALSYB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFYrPd2WsAALSYB.jpg",
+					"url": "https://t.co/MK3CKSHMJ8",
+					"display_url": "pic.twitter.com/MK3CKSHMJ8",
+					"expanded_url": "https://twitter.com/LeStreamFR/status/1177175903291478017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1177175897310466000,
+					"id_str": "1177175897310466054",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EFYrPd1XkAYjWI0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFYrPd1XkAYjWI0.jpg",
+					"url": "https://t.co/MK3CKSHMJ8",
+					"display_url": "pic.twitter.com/MK3CKSHMJ8",
+					"expanded_url": "https://twitter.com/LeStreamFR/status/1177175903291478017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 710885582780629000,
+			"id_str": "710885582780628996",
+			"name": "LeStream",
+			"screen_name": "LeStreamFR",
+			"location": "Paris, France",
+			"description": "Comment y va ?",
+			"url": "https://t.co/ngSE4oF1l0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ngSE4oF1l0",
+							"expanded_url": "http://www.twitch.tv/lestream",
+							"display_url": "twitch.tv/lestream",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 214282,
+			"friends_count": 414,
+			"listed_count": 126,
+			"created_at": "Fri Mar 18 17:48:25 +0000 2016",
+			"favourites_count": 6349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11819,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171100002581647360/RA4N5MLK_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171100002581647360/RA4N5MLK_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/710885582780628996/1568046974",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3412,
+		"favorite_count": 1036,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "#Concours 🎁\n\nTente de gagner ce mega Pack Dofus !  Pour participer : RT + FOLLOW @LeStreamFR 👌\n\nTAS : 03/10 https://t.co/MK3CKSHMJ8"
+	},
+	{
 		"created_at": "Sat Sep 14 06:29:03 +0000 2019",
 		"id": 1172759142878109700,
 		"id_str": "1172759142878109696",
