6fed227d4524aa618883e164eeac9f999aaf5227
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-10-29 01:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 202,
-	"created_at": "Mon Sep 30 00:21:06 +0000 2019",
+	"tweets": 203,
+	"created_at": "Tue Oct 29 00:21:08 +0000 2019",
 	"lang": "fr"
 }
---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Mon Oct 28 03:38:31 +0000 2019",
+		"id": 1188661292858577000,
+		"id_str": "1188661292858576896",
+		"full_text": "@GazetteLive34 : Gilles Lorillon l'aura-t-il dans le fion? https://t.co/0RiDIeSUgM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GazetteLive34",
+					"name": "Gazette Live Mtp",
+					"id": 469144681,
+					"id_str": "469144681",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1188661288693653500,
+					"id_str": "1188661288693653504",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/EH75JO7X4AAZfIc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EH75JO7X4AAZfIc.jpg",
+					"url": "https://t.co/0RiDIeSUgM",
+					"display_url": "pic.twitter.com/0RiDIeSUgM",
+					"expanded_url": "https://twitter.com/taggaddaaaa/status/1188661292858576896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 474,
+							"h": 267,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 474,
+							"h": 267,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 474,
+							"h": 267,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1188661288693653500,
+					"id_str": "1188661288693653504",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/EH75JO7X4AAZfIc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EH75JO7X4AAZfIc.jpg",
+					"url": "https://t.co/0RiDIeSUgM",
+					"display_url": "pic.twitter.com/0RiDIeSUgM",
+					"expanded_url": "https://twitter.com/taggaddaaaa/status/1188661292858576896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 474,
+							"h": 267,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 474,
+							"h": 267,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 474,
+							"h": 267,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 469144681,
+		"in_reply_to_user_id_str": "469144681",
+		"in_reply_to_screen_name": "GazetteLive34",
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "Taggaddaaaa",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier, France",
+			"description": "student @42born2code. Coding, and eating are my fav'passtime!",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 109,
+			"friends_count": 436,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 112,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 130,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/437199160161230848/BSryCr2t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1501158662",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "@GazetteLive34 : Gilles Lorillon l'aura-t-il dans le fion? https://t.co/0RiDIeSUgM"
+	}
+]
