a901d88a1cf5253a2d295178c652fb25d4fbe88d
taggaddaaaa <sabinecaizergues@hotmail.com>
2020-04-16 02:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 208,
-	"created_at": "Thu Dec 12 00:21:06 +0000 2019",
+	"tweets": 209,
+	"created_at": "Thu Apr 16 00:21:06 +0000 2020",
 	"lang": "fr"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2020_04.js",
+		"year": 2020,
+		"var_name": "tweets_2020_04",
+		"tweet_count": 1,
+		"month": 4
+	},
+	{
 		"file_name": "data/js/tweets/2019_12.js",
 		"year": 2019,
 		"var_name": "tweets_2019_12",
