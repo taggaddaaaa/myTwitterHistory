0906bc23c7a52082e6925ea1e168d47d534136d5
taggaddaaaa <sabinecaizergues@hotmail.com>
2020-11-05 01:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 212,
-	"created_at": "Sat May 02 00:21:06 +0000 2020",
+	"tweets": 213,
+	"created_at": "Thu Nov 05 00:21:06 +0000 2020",
 	"lang": "fr"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Wed Nov 04 09:59:43 +0000 2020",
+		"id": 1323927901591556000,
+		"id_str": "1323927901591556096",
+		"full_text": "Annonce STAGE Montpellier: Stage Business Development (H/F) - Montpellier\nhttps://t.co/hENQpHiPTZ \nSuivez : @MontpellierJobs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MontpellierJobs",
+					"name": "Emplois Montpellier",
+					"id": 1515012108,
+					"id_str": "1515012108",
+					"indices": [
+						108,
+						124
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/hENQpHiPTZ",
+					"expanded_url": "http://www.francesurf.net/offre-emploi-stage-business-development-montpellier_18428610.htm",
+					"display_url": "francesurf.net/offre-emploi-s…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://job.francesurf.net/\" rel=\"nofollow\">la recherche d'emploi par km</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2339848938,
+			"id_str": "2339848938",
+			"name": "sabine.caizergues",
+			"screen_name": "taggaddaaaa",
+			"location": "Montpellier et ailleurs 🌎",
+			"description": "🇫🇷 Passionnée de #strategiedigitale\n💡 Créatrice de solutions grâce aux outils numériques\n👩‍💼 Consultante SEO & Réseaux sociaux\n🎙Formatrice & Conférencière",
+			"url": "https://t.co/s4Le2mEWoR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s4Le2mEWoR",
+							"expanded_url": "http://www.sabinecaizergues.fr",
+							"display_url": "sabinecaizergues.fr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 108,
+			"friends_count": 449,
+			"listed_count": 9,
+			"created_at": "Wed Feb 12 06:18:07 +0000 2014",
+			"favourites_count": 126,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 97,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1244556838131073024/8Jg62WRd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1244556838131073024/8Jg62WRd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2339848938/1585560449",
+			"profile_link_color": "C50077",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Annonce STAGE Montpellier: Stage Business Development (H/F) - Montpellier\nhttps://t.co/hENQpHiPTZ \nSuivez : @MontpellierJobs"
+	}
+]
